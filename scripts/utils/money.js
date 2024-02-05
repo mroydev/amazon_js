cf8d590fd464b0dfbb659ea20532549bb775a960
M Roy <mroy.in21@gmail.com>
2024-02-05 10:32:23 +0530

    create Automated Tests for formatCurrency()

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,6 +1,7 @@
 export function formatCurrency(priceCents) {
-  return (priceCents / 100).toFixed(2);
+  return (Math.round(priceCents) / 100).toFixed(2);
 }
+
 export function formatCurrencyTotalCents(priceCents) {
   return Math.round(priceCents / 100).toFixed(2);
 }
